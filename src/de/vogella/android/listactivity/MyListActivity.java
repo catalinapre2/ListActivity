0410a6e2403a76248b9dc82b46bc0d108815184a
Catalina Predoi <s1006617@sms.ed.ac.uk>
2012-02-18 17:33:32 +0000

    Finally got MyPerformanceArrayAdapter to work

--- src/de/vogella/android/listactivity/MyListActivity.java
+++ src/de/vogella/android/listactivity/MyListActivity.java
@@ -3,16 +3,20 @@ package de.vogella.android.listactivity;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
+//import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class MyListActivity extends ListActivity {
+	
+	public static final String LOG_TAG = "Tag: Catalina";
+	
 	public void onCreate(Bundle icicle){
 		super.onCreate(icicle);
-		String[] values = new String[] { "Android", "iPhone", "WindowsMobile", "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2"};
-		MySimpleArrayAdapter adapter = new MySimpleArrayAdapter(this, values);
+		String[] values = new String[] { "Android", "iPhone", "WindowsMobile", "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2"};	
+		MyPerformanceArrayAdapter adapter = new MyPerformanceArrayAdapter(this, values);
 		setListAdapter(adapter);
+		
 	}
 
 
